fa74362857d20d208cc347085b6f95ee9efeb57e
Mango3313 <daft_noob3313@live.com>
2020-03-04 16:56:35 -0600

    Hola mundo express

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+var dotenv = require('dotenv');
 var app = express();
-app.get('/ora',(req,res)=>{
-    res.end("Exito");
+dotenv.config();
+app.get('/',(req,res,next)=>{
+    res.send("Bienvenido");
+});
+app.get('/:nombre',(req,res,next)=>{
+    //var nombre = req.query.nombre;
+    //res.status(200).json({message:"Exito, "+nombre});
+    res.send("Hola, "+req.params.nombre);
+});
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("App en el puerto 3000");
 });
