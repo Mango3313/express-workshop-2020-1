0cf3abf5a95569cd852429548028233948c23023
Mango3313 <daft_noob3313@live.com>
2020-03-30 01:17:53 -0600

    Pokedex POST y Postman

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 var dotenv = require('dotenv');
+const bodyparser = require('body-parser');
 //const Font = require('ascii-art-font');
 var {pokemon} = require('./PokedexData/pokedex.json');
 var app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
 dotenv.config();
 /**Codigo anterior
 app.get('/',(req,res,next)=>{
@@ -14,7 +19,12 @@ app.get('/:nombre',(req,res,next)=>{
     res.send("Hola, "+req.params.nombre);
 });
 **/
-
+app.get('/',(req,res,next)=>{
+    res.send("Beinvenido a la pokedex...");
+});
+app.post('/pokemon',(req,res,next)=>{
+    res.status(200).send("Estas en /pokemon POST");
+});
 app.get('/pokemon',(req,res,next)=>{
     /** No pude escribir pokedex en ASCII uwu
      * 
@@ -24,18 +34,27 @@ app.get('/pokemon',(req,res,next)=>{
     });
      * 
      */
-    res.send("Bienvenido a la pokedex");
+    res.status(200).send(pokemon);
+});
+app.post('/pokemon',(req,res,next)=>{
+    var idpoke = req.body.id - 1;
+    (idpoke > 0 && idpoke < 150) ? res.status(200).send(pokemon[idpoke]) : res.status(404).send("Pokemon no existe");
 });
 app.get('/pokemon/:id([0-9]{1,3})',(req,res,next)=>{
     var idpoke = req.params.id - 1;
-    if (idpoke > 0 && idpoke < 150){
-        res.status(200).send(pokemon[idpoke]);
-    }else{
-        res.status(404).send("Pokemon no existe");
-    }
+    (idpoke > 0 && idpoke < 150) ? res.status(200).send(pokemon[idpoke]) : res.status(404).send("Pokemon no existe");
 });
-app.get('/pokemon/:name',(req,res,next)=>{
+app.post('/pokemon',(req,res,next)=>{
+    var pokename = req.body.name;
+    var monster = pokemon.filter(p => p.name.toLowerCase() == pokename.toLowerCase());
+    (monster.length > 0) ? res.status(200).send(monster) : res.status(404).send("Pokemon no encontrado") ;
+});
+app.get('/pokemon/:name([A-Za-z]+)',(req,res,next)=>{
     var pokename = req.params.name;
+    var monster = pokemon.filter(p => p.name.toLowerCase() == pokename.toLowerCase());
+    (monster.length > 0) ? res.status(200).send(monster) : res.status(404).send("Pokemon no encontrado") ;
+    /**
+     * var pokename = req.params.name;
     var encontrado = false;
    for(let element of pokemon ){
         if (element.name == pokename){
@@ -47,6 +66,8 @@ app.get('/pokemon/:name',(req,res,next)=>{
     if(!encontrado){
         res.status(404).send("Pokemon no encontrado");
     }
+     */
+    
 });
 app.listen(process.env.PORT || 3000,()=>{
     console.log("App en el puerto 3000");
